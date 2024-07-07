c6cb988925abaf2d8c5ef78999e1d8bf2614e1e7
Subhendu <subho30666@gmail.com>
2024-07-07 09:51:36 +0530

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,25 @@ import ProjectSettings from "./Pages/Project/ProjectSettings";
 import IssuesOnClickModal from "./Pages/IssuesOnClickModal";
 import { ToastProvider } from "./components/Toast";
 function App() {
-	return (
-		<>
-			<ToastProvider>
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<Home />}></Route>
-						<Route path="/project" element={<NavbarOutlet />}>
-							<Route path="/project" element={<Project />}>
-								<Route path="/project/board" element={<Board />}>
-									<Route path="/project/board/:items" element={<IssuesOnClickModal />} />
-								</Route>
-								<Route path="/project/settings" element={<ProjectSettings />} />
-							</Route>
-						</Route>
-					</Routes>
-				</BrowserRouter>
-			</ToastProvider>
-		</>
-	);
+  return (
+    <>
+      <ToastProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/jira_clone" element={<Home />}></Route>
+            <Route path="/project" element={<NavbarOutlet />}>
+              <Route path="/project" element={<Project />}>
+                <Route path="/project/board" element={<Board />}>
+                  <Route path="/project/board/:items" element={<IssuesOnClickModal />} />
+                </Route>
+                <Route path="/project/settings" element={<ProjectSettings />} />
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ToastProvider>
+    </>
+  );
 }
 
 export default App;
