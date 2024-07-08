bc81254a3df0b03a6e74eb842e76f6f6366ef123
Subhendu <16267867+subho30666@users.noreply.github.com>
2024-07-08 05:32:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Replace with your repository name
+  base: "/jira_clone/", // Set to your GitHub repository name
 });
